4a70b50a24132c9d533f3112a361989a17c4cef8
7x <info@se7enx.com>
2024-02-05 05:51:50 +0000

    Added: Added checkout of latest bcwebshop extension

---
+++ packages/sevenx_extension/ezextension/bcwebshop/ezinfo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File containing the bcwebshopInfo class.
+ *
+ * @copyright Copyright (C) 1999 - 2012 7x. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or any later version)
+ * @version //autogentag//
+ * @package bcimagealias
+ */
+class bcwebshopInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "<a href='https://github.com/bcwebshop'>BC Web Shop</a>",
+            'Version' => "0.8.1",
+            'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com'>7x</a> and <a href='https://brookinsconsulting.com'>Brookins Consulting</a> and <a href='https://ibexa.no'>eZ Systems AS</a>",
+            'Author' => "7x",
+            'License' => "GNU General Public License"
+        );
+    }
+}
+?>
