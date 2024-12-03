ebb5ded0153a05fa631e87f7b19ed94a056f83eb
7x <info@se7enx.com>
2024-12-03 14:37:06 -0800

    Renamed: Renamed primary design extension to match composer package new to 6.0.6

---
+++ packages/sevenx_extension/ezextension/sevenx_themes_simple/settings/design.ini.append.php
@@ -0,0 +1,31 @@
+<?php /* #?ini charset="iso-8859-1"?
+
+[ExtensionSettings]
+DesignExtensions[]=sevenx_themes_simple
+
+[StylesheetSettings]
+#SiteCSS=var/storage/packages/eZ-systems/ezwebin_design_gray/files/default/file/site-colors.css
+#ClassesCSS=var/storage/packages/eZ-systems/ezwebin_design_gray/files/default/file/classes-colors.css
+#CSSFileList[]=yui/build/calendar/assets/calendar.css
+#CSSFileList[]=site-colors.css
+#CSSFileList[]=project.css
+CSSFileList[]=websitetoolbar.css
+CSSFileList[]=libs/fontawesome/css/all.min.css
+#CSSFileList[]=libs/splide/css/splide.min.css
+CSSFileList[]=magnific-popup.css
+CSSFileList[]=main.css
+CSSFileList[]=main.res.css
+
+[JavaScriptSettings]
+#JavaScriptList[]=jquery/jquery-3.7.0.min.js
+#JavaScriptList[]=insertmedia.js
+#JavaScriptList[]=yui/build/yahoo-dom-event/yahoo-dom-event.js
+#JavaScriptList[]=yui/build/calendar/calendar.js
+#JavaScriptList[]=ezdatepicker.js
+JavaScriptList[]=ezjsc::jquery
+JavaScriptList[]=jquery.magnific-popup.js
+#JavaScriptList[]=splide/js/splide.min.js
+#JavaScriptList[]=splide/extensions/splide-extension-auto-scroll.min.js
+JavaScriptList[]=main.js
+
+*/ ?>
